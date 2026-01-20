593e23c2c02c146222f29db3aec265abbfaee1f3
n_popov <npopov2@yandex.com>
2026-01-20 08:22:53 +0300

    changed Выйти on Вход

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class UserCreateForm(UserCreationForm):
     username = forms.CharField(
         label="Имя пользователя",
