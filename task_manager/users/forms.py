843d6b00015d6be6269052ab24a4a3657e0ad2c5
n_popov <npopov2@yandex.com>
2026-01-21 07:38:55 +0300

    make talor for hexlet

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -45,10 +45,27 @@ class UserUpdateForm(forms.ModelForm):
         ),
         help_text='Обязательно. Не более 150 символов. '
                   'Только буквы, цифры и @/./+/-/_')
+    password1 = forms.CharField(
+        label="Пароль",
+        help_text='Ваш пароль должен содержать как минимум 3 символа.',
+        widget=forms.PasswordInput(
+            attrs={"placeholder": "Пароль"}
+        ),
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        help_text='Для подтверждения введите, пожалуйста, пароль ещё раз.',
+        widget=forms.PasswordInput,
+    )
+
     class Meta:
         model = User
-        fields = ("username", "first_name", "last_name",)
+        fields = ("first_name", "last_name", "username", "password1", "password2")
         labels = {
+            "username": "Имя пользователя",
             "first_name": "Имя",
             "last_name": "Фамилия",
+            "password1": "Пароль",
+            "password2": "Подтверждение пароля",
+
         }
