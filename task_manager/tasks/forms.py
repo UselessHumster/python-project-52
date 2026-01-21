fd8c7ff9f9c5a94daa8f2bdbb77e258bcab706d3
n_popov <npopov2@yandex.com>
2026-01-21 18:33:35 +0300

    make talor for hexlet

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -8,7 +8,11 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ["name", "description", "status", "executor", "labels"]
         widgets = {
-            "status": forms.Select(attrs={"class": "form-select"}),
-            "executor": forms.Select(attrs={"class": "form-select"}),
-            'labels': forms.SelectMultiple(attrs={'class': 'form-control'})
+            "labels": forms.SelectMultiple(attrs={"class": "form-control"}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["executor"].empty_label = "Исполнитель"
+        self.fields["status"].empty_label = "Статус"
