b696726f9e7fb9f347aabb02c03f86b042b81f3a
n_popov <npopov2@yandex.com>
2026-01-22 08:15:35 +0300

    make talor for hexlet

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView,
@@ -67,6 +68,10 @@ class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         return self.request.user == self.get_object().author
+
+    def handle_no_permission(self):
+        messages.error(self.request, "У вас нет прав для изменения задач другого пользователя.")
+        return redirect("task_list")
 
     def form_valid(self, form):
         messages.success(self.request, "Задача успешно удалена")
